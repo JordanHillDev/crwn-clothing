25c25f9ea3f0e84c73af961300f8fe7802c46d03
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2021-06-14 16:44:24 -0400

    put signInWithEmail and signInWithGoogle in sagas

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-import { CollectionItemContainer } from './collection-item.styles';
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer
+} from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
 
   return (
     <CollectionItemContainer>
-      <div
-        className='image'
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <BackgroundImage className='image' imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price}</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton onClick={() => addItem(item)} inverted>
         Add to cart
-      </CustomButton>
+      </AddButton>
     </CollectionItemContainer>
   );
 };
--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import CustomButton from '../custom-button/custom-button.component';
 
 export const CollectionItemContainer = styled.div`
   width: 22vw;
@@ -8,47 +9,49 @@ export const CollectionItemContainer = styled.div`
   align-items: center;
   position: relative;
 
-  .image {
-    width: 100%;
-    height: 95%;
-    background-size: cover;
-    background-position: center;
-    margin-bottom: 5px;
-  }
-
-  .custom-button {
-    width: 80%;
-    opacity: 0.7;
-    position: absolute;
-    top: 255px;
-    display: none;
-  }
-
   &:hover {
     .image {
       opacity: 0.8;
     }
 
-    .custom-button {
+    button {
       opacity: 0.85;
       display: flex;
     }
   }
+`;
 
-  .collection-footer {
-    width: 100%;
-    height: 5%;
-    display: flex;
-    justify-content: space-between;
-    font-size: 18px;
+export const AddButton = styled(CustomButton)`
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 255px;
+  display: none;
+`;
 
-    .name {
-      width: 90%;
-      margin-bottom: 15px;
-    }
+export const BackgroundImage = styled.div`
+  width: 100%;
+  height: 95%;
+  background-size: cover;
+  background-position: center;
+  margin-bottom: 5px;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+`;
 
-    .price {
-      width: 10%;
-    }
-  }
+export const CollectionFooterContainer = styled.div`
+  width: 100%;
+  height: 5%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+`;
+
+export const NameContainer = styled.span`
+  width: 90%;
+  margin-bottom: 15px;
+`;
+
+export const PriceContainer = styled.span`
+  width: 10%;
+  text-align: right;
 `;
